f5a07e1f2dab616490dc6f77c3ab35d40cf287a0
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-05-08 07:03:19 -0400

    added notes

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,14 +1,14 @@
 const repeatString = function() {
-    let userString = prompt ("Enter a noun: ");
-    let userNumber = prompt ("Enter a whole number: ");
-    let numberUsed = parseInt (userNumber);
+    let userString = prompt ("Enter a noun: "); //ask user for a noun
+    let userNumber = prompt ("Enter a whole number: "); //ask user for a whole number
+    let numberUsed = parseInt (userNumber); //converts user input to int
 
-    let repeatedString = [userString * numberUsed];
+    let repeatedString = [userString * numberUsed]; // goal is to create an array which consists of the users string repated by the users number
 
-    if (i == [-1]){
+    if (i == [-1]){ // if array space is -1 remove , after last iterration
         alert ("  .")
         else
-            alert (" ")
+            alert (" ") // if array is any other index just output text with coma
     };
 
 };
