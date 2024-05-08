ebc7265931df5b888db806bb8527e880211bd179
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-05-08 06:43:54 -0400

    array bones

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,6 +2,8 @@ const repeatString = function() {
     let userString = prompt ("Enter a noun: ");
     let userNumber = prompt ("Enter a whole number: ");
     let numberUsed = parseInt (userNumber);
+
+    repeatedString = [userString * numberUsed]
 };
 
 // Do not edit below this line
