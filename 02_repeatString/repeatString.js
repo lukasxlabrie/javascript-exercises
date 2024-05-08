2147b841264e8bcaeb6e8e60f4619d2b6823ee35
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-05-08 07:03:12 -0400

    function bones

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -3,7 +3,14 @@ const repeatString = function() {
     let userNumber = prompt ("Enter a whole number: ");
     let numberUsed = parseInt (userNumber);
 
-    repeatedString = [userString * numberUsed]
+    let repeatedString = [userString * numberUsed];
+
+    if (i == [-1]){
+        alert ("  .")
+        else
+            alert (" ")
+    };
+
 };
 
 // Do not edit below this line
