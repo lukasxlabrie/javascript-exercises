d75db63856e5cf6f458aa65e50a263b07aa3fd84
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-05-08 13:59:36 -0400

    used .repeat (can't use *)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -3,7 +3,7 @@ const repeatString = function() {
     let userNumber = prompt ("Enter a whole number: "); //ask user for a whole number
     let numberUsed = parseInt (userNumber); //converts user input to int
 
-    let stringRepeater = userString * numberUsed; // creates 3 strings
+    let stringRepeater = userString.repeat(numberUsed); // creates 3 strings
 
     let repeatedString = []; // empty array
     stringRepeater.push(repeatedString); //take output and adds to array
