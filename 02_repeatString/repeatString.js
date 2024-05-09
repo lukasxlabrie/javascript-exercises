acf72fbb0382b592c759775fbf86f893a0247161
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-05-09 06:30:29 -0400

    changed names for clarity

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,9 @@
 const repeatString = function() {
-    let userString = prompt ("Enter a noun: "); //ask user for a noun
-    let userNumber = prompt ("Enter a whole number: "); //ask user for a whole number
-    let numberUsed = parseInt (userNumber); //converts user input to int
+    let inoputString = prompt ("Enter a noun: "); //ask user for a noun
+    let inputrNumber = prompt ("Enter a whole number: "); //ask user for a whole number
+    let userNumber = parseInt (inputNumber); //converts user input to int
 
-    let stringRepeater = userString.repeat(numberUsed); // creates 3 strings
+    let stringRepeater = inputString.repeat(userNumber); // repeats string by the number entered
 
     let repeatedString = []; // empty array
     stringRepeater.push(repeatedString); //take output and adds to array
