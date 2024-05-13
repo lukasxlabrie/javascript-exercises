9c25e3d2d3ae310c138b6509abf541685a60e2fc
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-05-13 06:32:28 -0400

    changed from noun to just a word

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,5 @@
 const repeatString = function() {
-    let inputString = prompt ("Enter a noun: "); //ask user for a noun
+    let inputString = prompt ("Enter a word: "); //ask user for a word
     let inputNumber = prompt ("Enter a whole number: "); //ask user for a whole number
     let userNumber = parseInt (inputNumber); //converts user input to int
 
