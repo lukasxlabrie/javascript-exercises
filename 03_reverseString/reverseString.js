f73ba519141bc6df4c1d5d95c68affbafc490b13
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-05-20 06:36:56 -0400

    addded notes

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,7 +1,7 @@
-const reverseString = function(word) {
+const reverseString = function(word)  { //states function needs 1 arguement in this case its word
 
-    wordStorage = [];
-    word.push(wordStorage)
+    wordStorage = []; //empty array
+    word.push(wordStorage) // takes string and puts it into the array
 };
 
 // Do not edit below this line
