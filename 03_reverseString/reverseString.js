1161db74aace11822b10964307c71b8e140df13b
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-05-20 07:02:37 -0400

    whitespace

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -3,8 +3,6 @@ const reverseString = function(word)  { //states function needs 1 arguement in t
     wordStorage = []; //empty array
     wordStorage.push(word) // takes string and puts it into the array
     
-
-    
 };
 
 // Do not edit below this line
