90675d7a226b61667e6ce9f33053f40fbfc425ea
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-05-20 06:38:53 -0400

    sytnax correction

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,7 +1,7 @@
 const reverseString = function(word)  { //states function needs 1 arguement in this case its word
 
     wordStorage = []; //empty array
-    word.push(wordStorage) // takes string and puts it into the array
+    wordStorage.push(word) // takes string and puts it into the array
 };
 
 // Do not edit below this line
