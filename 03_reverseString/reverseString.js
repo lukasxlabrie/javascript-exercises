f2886404483240b4422e591542f5492be9803134
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-05-21 06:44:04 -0400

    added to note

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -5,7 +5,7 @@ const reverseString = function(word)  { //states function needs 1 arguement in t
 
     wordReverse = [] // empty array
     
-    let i = wordStorage.length -1; // starts loop
+    let i = wordStorage.length -1; // starts loop at the end if array
     while (i >= 0) { // will run while i is greater than or eqaul to 0
         wordReverse.push(char[i]) // will move current char to new array
     i--
