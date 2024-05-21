9bbe89c0c15a8e6457b8165ad77a8764b97fbe13
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-05-21 06:49:30 -0400

    added new var to store new word

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -3,15 +3,15 @@ const reverseString = function(word)  { //states function needs 1 arguement in t
     wordStorage = []; //empty array
     wordStorage.push(word) // takes string and puts it into the array
 
-    wordReverse = [] // empty array
+    wordReverseStorage = [] // empty array
     
     let i = wordStorage.length -1; // starts loop at the end if array
     while (i >= 0) { // will run while i is greater than or eqaul to 0
-        wordReverse.push(char[i]) // will move current char to new array
+        wordReverseStorage.push(char[i]) // will move current char to new array
     i--
     }
 
-
+    let wordReverse = wordReverseStorage.join (" "); // combines elementd in array and removes ,
 };
 
 // Do not edit below this line
