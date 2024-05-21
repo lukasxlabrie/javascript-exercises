5f4aa56da69349e7731040567b99f941be1024b1
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-05-21 06:42:41 -0400

    rewrote while loop, added new array

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,11 +2,16 @@ const reverseString = function(word)  { //states function needs 1 arguement in t
 
     wordStorage = []; //empty array
     wordStorage.push(word) // takes string and puts it into the array
-    
-    let i = 0 // starts loop
-    while (i < wordStorage.length) { //while loop, since we do not know how many runs to complete
 
+    wordReverse = [] // empty array
+    
+    let i = wordStorage.length -1; // starts loop
+    while (i >= 0) { // will run while i is greater than or eqaul to 0
+        wordReverse.push(char[i]) // will move current char to new array
+    i--
     }
+
+
 };
 
 // Do not edit below this line
