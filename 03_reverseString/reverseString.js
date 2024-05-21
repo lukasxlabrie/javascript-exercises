1e9e4ecfb3fb1aa9d6ffebb5a2708b8efc2307eb
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-05-21 06:52:31 -0400

    removed char

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -7,7 +7,7 @@ const reverseString = function(word)  { //states function needs 1 arguement in t
     
     let i = wordStorage.length -1; // starts loop at the end if array
     while (i >= 0) { // will run while i is greater than or eqaul to 0
-        wordReverseStorage.push(char[i]) // will move current char to new array
+        wordReverseStorage.push([i]) // will move current char to new array
     i--
     }
 
