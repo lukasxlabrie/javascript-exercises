188e05674ef6df3f5fad3498779688fa9ceac31f
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-05-22 06:50:56 -0400

    removed first array

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,8 @@
 const reverseString = function(inputString)  { //states function needs 1 arguement in this case its a string
 
-    wordStorage = []; //empty array
-    wordStorage.push(inputString) // takes string and puts it into the array
-
     wordReverseStorage = [] // empty array
     
-    let i = wordStorage.length -1; // starts loop at the end if array
+    let i = inputString.length -1; // starts loop at the end if array
     while (i >= 0) { // will run while i is greater than or eqaul to 0
         wordReverseStorage.push([i]) // will move current char to new array
     i--
