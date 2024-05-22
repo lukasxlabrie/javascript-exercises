20163b04a2e622c044a3c9f2353bc0fe2e1c7ee4
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-05-22 13:29:03 -0400

    moved }, just to fix whitespace

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -6,7 +6,7 @@ const reverseString = function(input)  { //states function needs 1 arguement in 
     while (i >= 0) { // will run while i is greater than or eqaul to 0
         wordReverseStorage.push(inputString.charAt[i]) // will move current char to new array
     i--;
-    }
+}
 
     let wordReverse = wordReverseStorage.join (" "); // combines elements in array and removes ,
     console.log (wordReverse)
