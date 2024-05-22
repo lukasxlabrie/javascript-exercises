f1c74c101ec47ba55b9d2f9c9b6aef08d364ee70
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-05-22 06:31:06 -0400

    removed name of argument

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,7 +1,7 @@
-const reverseString = function(word)  { //states function needs 1 arguement in this case its word
+const reverseString = function()  { //states function needs 1 arguement in this case its word
 
     wordStorage = []; //empty array
-    wordStorage.push(word) // takes string and puts it into the array
+    wordStorage.push() // takes string and puts it into the array
 
     wordReverseStorage = [] // empty array
     
