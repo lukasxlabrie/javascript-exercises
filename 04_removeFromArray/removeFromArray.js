ed8a0b23184f44784ab8df615a00eb9e46f9aced
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-05-28 07:04:47 -0400

    added notes and function call

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,12 @@
 const removeFromArray = function(array, input) { 
     //will use an array and user input
-    
+
 
 
 };
+
+removeFromArray([1,2,3,4], 3)
+// this means use the function to remove ", 3 " from the array [1, 2, 3, 4]
 
 // Do not edit below this line
 module.exports = removeFromArray;
