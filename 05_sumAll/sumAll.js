e6a36e2aec933ab0e3409590d3dd79d28b2b58c3
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-06-30 13:30:03 -0400

    defined vars, while loop, and return values

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+numBetween = [];
+let i = 0;
+while (num2 < num1, i++){
+    numBetween.push(charAt[i])
+};
+sumTotal = numBetween + num1 + num2;
+return sumAll;
 };
 
 // Do not edit below this line
