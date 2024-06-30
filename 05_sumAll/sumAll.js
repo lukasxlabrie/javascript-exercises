c8237766794c92a5a37b0f74dfa9493e3505ea50
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-06-30 13:34:21 -0400

    added notes

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,11 @@
-const sumAll = function(num1, num2) {
-numBetween = [];
-let i = 0;
-while (num2 < num1, i++){
-    numBetween.push(charAt[i])
+const sumAll = function(num1, num2) { // states that thi sfunction will need 2 inputs
+numBetween = []; // empty array to be used to store numbers between 2 inputs
+let i = 0; // to start iterrations
+while (num2 < num1, i++){ //will run while num 2 is less than num1
+    numBetween.push(charAt[i]) //takes each iterration and saves it in a new array
 };
-sumTotal = numBetween + num1 + num2;
-return sumAll;
+sumTotal = numBetween + num1 + num2; // adds the array and inputs
+return sumAll; // prints the sum 
 };
 
 // Do not edit below this line
