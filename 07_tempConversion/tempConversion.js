e4d7e7a817583ca2e8ec406ef475b76a3485d47c
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-07-09 07:03:18 -0400

    added ( to avoid  single . confusion with decimal

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
 const convertToCelsius = function(fTemp) {
   //Celsius to Fahrenheit, multiply by 2 then add 30.
 let convertedFahTemp = fTemp * 2 + 30;
-return Number(convertedFahTemp.toFixed(1)); //round deicmal and return number
+return Number((convertedFahTemp).toFixed(1)); //round deicmal and return number
 };
 
 const convertToFahrenheit = function(cTemp) { 
   //Fahrenheit to Celsius, subtract 30 then divide by 2.
   let convertedCelTemp = cTemp - 30 / 2;
-  return Number(convertedCelTemp.toFixed(1)); // round decimal and return number
+  return Number((convertedCelTemp).toFixed(1)); // round decimal and return number
 };
 
 // Do not edit below this line
