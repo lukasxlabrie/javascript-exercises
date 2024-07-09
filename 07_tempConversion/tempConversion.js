555a9cdc90a5239e8efb765ea3d5e222d8c99423
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-07-09 06:49:06 -0400

    coded formulas

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,13 @@
 const convertToCelsius = function(fTemp) {
   //Celsius to Fahrenheit, multiply by 2 then add 30.
-
+let convertedFahTemp = fTemp * 2 + 30;
+return convertedFahTemp;
 };
 
-const convertToFahrenheit = function(Ctemp) { 
+const convertToFahrenheit = function(cTemp) { 
   //Fahrenheit to Celsius, subtract 30 then divide by 2.
+  let convertedCelTemp = cTemp - 30 / 2;
+  return convertedCelTemp;
 };
 
 // Do not edit below this line
