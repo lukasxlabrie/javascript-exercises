04d11d003a9d4b94f8db1efc4e3df10d09c19e0a
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-07-09 06:56:17 -0400

    added method and notes for rounding decimals

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
 const convertToCelsius = function(fTemp) {
   //Celsius to Fahrenheit, multiply by 2 then add 30.
 let convertedFahTemp = fTemp * 2 + 30;
-return convertedFahTemp;
+return convertedFahTemp.toFixed(2); //round deicmal
 };
 
 const convertToFahrenheit = function(cTemp) { 
   //Fahrenheit to Celsius, subtract 30 then divide by 2.
   let convertedCelTemp = cTemp - 30 / 2;
-  return convertedCelTemp;
+  return convertedCelTemp.toFixed(2); // round decimal
 };
 
 // Do not edit below this line
